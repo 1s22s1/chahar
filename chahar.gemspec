eb9e476c13e65af7a1a4bbe92cd31fe7014ccec2
1s22s1 <1s22s1@example.com>
2023-09-27 07:40:47 +0900

    Add more test

--- chahar.gemspec
+++ chahar.gemspec
@@ -12,7 +12,7 @@ Gem::Specification.new do |spec|
   spec.description = 'This library provides the Set class.'
   spec.homepage = 'https://github.com/1s22s1/chahar'
   spec.license = 'MIT'
-  spec.required_ruby_version = '>= 2.6.0'
+  spec.required_ruby_version = '>= 3.2.0'
 
   spec.metadata['homepage_uri'] = spec.homepage
   spec.metadata['source_code_uri'] = 'https://github.com/1s22s1/chahar'
--- spec/chahar_spec.rb
+++ spec/chahar_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe Chahar do
   describe '#initialize' do
     it { expect { described_class.new }.not_to raise_error }
     it { expect { described_class.new(nil) }.not_to raise_error }
-    it { expect { described_class.new([1]) }.not_to raise_error }
+    it { expect { described_class.new([1, 2]) }.not_to raise_error }
+    it { expect { described_class.new(1..3) }.not_to raise_error }
   end
 
   describe '#size' do
     it { expect(described_class.new.size).to eq 0 }
     it { expect(described_class.new(nil).size).to eq 0 }
-    it { expect(described_class.new([1]).size).to eq 1 }
+    it { expect(described_class.new([1, 2]).size).to eq 2 }
+    it { expect(described_class.new(1..3).size).to eq 3 }
   end
 end
